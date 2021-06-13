6273f0e4663e4ccba0e147fef90420b4d0a47e6b
Diego Cardoso <diegocar448@hotmail.com>
2021-06-13 01:41:07 -0300

    Estruturando a camada Service

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -3,7 +3,9 @@ package one.digitalinnovation.personapi.controller;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /*RestController significa que é um controller de uma API Rest*/
@@ -12,26 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
-    /*@Autowired Aqui ele injeta uma implementação do tipo PersonRepository*/
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
-    /*Mapeando uma rota com o verbo HTTP do tipo GET/POST(para criação)*/
-    //@GetMapping
     @PostMapping
-    /* Passamos o corpo da requisição no parâmetro do método
-        e informamos que esta passando no corpo da Requisição com @RequestBody
-     */
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 
     /*BD JDBC URL = jdbc:h2:mem:testdb*/
