dd8644a389d8e910ed23db9eaaae49d88b2c0511
Diego Cardoso <diegocar448@hotmail.com>
2021-06-13 00:38:20 -0300

    Estruturando DTO | Repository | Controller e usando PostMapping

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,8 +1,10 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /*RestController significa que é um controller de uma API Rest*/
 @RestController
@@ -10,11 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    /*Mapeando uma rota com o verbo HTTP do tipo GET*/
-    @GetMapping
-    public String getBook(){
-        return "API Test!";
+    private PersonRepository personRepository;
+
+    /*@Autowired Aqui ele injeta uma implementação do tipo PersonRepository*/
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
+
+    /*Mapeando uma rota com o verbo HTTP do tipo GET/POST(para criação)*/
+    //@GetMapping
+    @PostMapping
+    /* Passamos o corpo da requisição no parâmetro do método
+        e informamos que esta passando no corpo da Requisição com @RequestBody
+     */
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+
+    /*BD JDBC URL = jdbc:h2:mem:testdb*/
 
 
 
