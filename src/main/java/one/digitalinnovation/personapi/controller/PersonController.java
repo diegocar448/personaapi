d395ccdc359c7b1d3d7b8b288d0b1b7331ce8a30
Diego Cardoso <diegocar448@hotmail.com>
2021-06-14 12:11:46 -0300

    fix

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -3,9 +3,12 @@ package one.digitalinnovation.personapi.controller;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+<<<<<<< HEAD
 import one.digitalinnovation.personapi.service.PersonService;
+=======
+import one.digitalinnovation.personapi.repository.PersonRepository;
+>>>>>>> parent of 6273f0e (Estruturando a camada Service)
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,17 +20,32 @@ import javax.validation.Valid;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonService personService;
+    private PersonRepository personRepository;
 
+    /*@Autowired Aqui ele injeta uma implementação do tipo PersonRepository*/
     @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
 
+    /*Mapeando uma rota com o verbo HTTP do tipo GET/POST(para criação)*/
+    //@GetMapping
     @PostMapping
+<<<<<<< HEAD
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+=======
+    /* Passamos o corpo da requisição no parâmetro do método
+        e informamos que esta passando no corpo da Requisição com @RequestBody
+     */
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+>>>>>>> parent of 6273f0e (Estruturando a camada Service)
     }
 
     /*BD JDBC URL = jdbc:h2:mem:testdb*/
