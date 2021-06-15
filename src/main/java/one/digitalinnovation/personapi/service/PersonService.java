48b72f98b7a51d31ca085947e69e11e48c4eeacd
Diego Cardoso <diegocar448@hotmail.com>
2021-06-14 21:06:54 -0300

    test building

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,16 +3,18 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 /*@Service indica que essa camada terá todas regras de negocio*/
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
@@ -26,14 +28,9 @@ public class PersonService {
      */
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         /*Fazer a conversao de DTO para Pessoas*/
-        Person personToSave = Person.builder()
-                .firstName(personDTO.getFirstName())
-                .lastName(personDTO.getLastName())
-                .birthDate(personDTO.getBirthDate())
-                .phones(personDTO.getPhones())
-                .build();
+        Person personToSave = personMapper.toModel(personDTO);
 
-        Person savedPerson = personRepository.save(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
