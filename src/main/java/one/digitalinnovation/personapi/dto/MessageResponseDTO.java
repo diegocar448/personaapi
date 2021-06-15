c09b7a2ba59556c5e89d543bccfaaea35782d8fd
Diego Cardoso <diegocar448@hotmail.com>
2021-06-15 15:59:47 -0300

    rollback

--- src/main/java/one/digitalinnovation/personapi/dto/MessageResponseDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/MessageResponseDTO.java
@@ -7,7 +7,6 @@ import lombok.Data;
 @Data
 @Builder
 public class MessageResponseDTO {
-    
 
     private String message;
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,18 +3,16 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
-import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /*@Service indica que essa camada terá todas regras de negocio*/
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
-
-    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
@@ -28,9 +26,14 @@ public class PersonService {
      */
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         /*Fazer a conversao de DTO para Pessoas*/
-        Person personToSave = personMapper.toModel(personDTO);
+        Person personToSave = Person.builder()
+                .firstName(personDTO.getFirstName())
+                .lastName(personDTO.getLastName())
+                .birthDate(personDTO.getBirthDate())
+                .phones(personDTO.getPhones())
+                .build();
 
-        Person savedPerson = personRepository.save(personToSave);
+        Person savedPerson = personRepository.save(personDTO);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
