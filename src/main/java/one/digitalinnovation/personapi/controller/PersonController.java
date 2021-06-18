655d7a56a3435242a0d054e62b985457f74de826
Diego Cardoso <diegocar448@hotmail.com>
2021-06-18 10:52:48 -0300

    F6 S

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,17 +1,32 @@
 package one.digitalinnovation.personapi.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API Test!";
+    private PersonRepository personRepository;
+
+    /*@Autowired injeta uma implementação do tipo Repository*/
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    /*@RequestBody informamos que vamos passar os dados do tipo Person pelo corpo da requisição*/
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
         //jdbc:h2:~/test
         //spring.datasource.url=jdbc:h2:mem:testdb
     }
