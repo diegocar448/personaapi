48f431929e627429f6f37ebe976e14215bb5ae2f
Diego Cardoso <diegocar448@hotmail.com>
2021-06-18 14:22:07 -0300

    F8 F

---
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.personapi.mapper;
+
+import one.digitalinnovation.personapi.dto.response.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+/*@Mapper anotação que indica que a classe mapper irá processar*/
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    /*Aqui definimos que a data terá apenas dia, mês e ano*/
+    /*@Mapping informamos o alvo*/
+    @Mapping(target = "birthDay", source = "birthDate", dateFormat = "dd-MM-yyyy")
+
+    /*toModel é uma convenção do mapStrict*/
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.response.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     /*@Autowired injeta uma implementação do tipo Repository*/
     @Autowired
     public PersonService(PersonRepository personRepository) {
@@ -23,14 +26,11 @@ public class PersonService {
 
     @PostMapping
     public MessageResponseDTO createPerson(PersonDTO personDTO){
-        Person personToSave = Person.builder()
-                .firstName(personDTO.getFirstName())
-                .lastName(personDTO.getLastName())
-                .birthDate(personDTO.getBirthDate())
-                .phones(personDTO.getPhones())
-                .build();
+        System.out.println("ENNNNNNNNNNNNNNNNNNTRRRRRRRRRRRRRRROOOUUUUUUUUUUUUUUUUUU");
+        Person personToSave = personMapper.toModel(personDTO);
 
-        Person savedPerson = personRepository.save(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
